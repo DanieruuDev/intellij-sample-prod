a31844b3f3e78903341fb3aef3ce695c9891b6cc
Danieruu <panturasd@gmail.com>
2026-01-14 00:02:10 +0800

    fix sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
-CREATE TABLE todo (
-                      id INT AUTO_INCREMENT PRIMARY KEY,
-                      content VARCHAR(255) NOT NULL,
-                      status VARCHAR(50) NOT NULL,
-                      created_at TIMESTAMP NOT NULL,
-                      updated_at TIMESTAMP
-);
+CREATE TABLE IF NOT EXISTS todo (
+    id SERIAL PRIMARY KEY,  -- SERIAL auto-increments like AUTO_INCREMENT in MySQL
+    content VARCHAR(255) NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+    );
